475218229c057bce1fcfc88fbc9d9a3660581ad4
Dénes Derhán <denes.derhan@gmail.com>
2025-08-21 20:48:16 +0200

    Removed handle_error() forced abort() in Debug builds, user can use assert() if needed.

--- fstlog/include/fstlog/detail/error_handling.hpp
+++ fstlog/include/fstlog/detail/error_handling.hpp
@@ -5,31 +5,10 @@
 #include <fstlog/detail/noexceptions.hpp>
 
 #ifdef FSTLOG_NOEXCEPTIONS
-#ifndef NDEBUG
-#include <cstdlib>
-#include <cstdio>
-
-namespace fstlog {
-    inline void handle_error(error_code error) noexcept {
-        if (error != error_code::none) {
-            std::fprintf(stderr, "Assertion failed: %s\n", error_message(error));
-            std::abort();
-        }
-    }
-
-    inline void error_if(bool condition, error_code error) noexcept {
-        if (condition) {
-            std::fprintf(stderr, "Assertion failed: %s\n", error_message(error));
-            std::abort();
-        }
-    }
-}
-#else
 namespace fstlog {
     inline void handle_error([[maybe_unused]] error_code error) noexcept {}
     inline void error_if([[maybe_unused]] bool condition, [[maybe_unused]] error_code error) noexcept {}
 }
-#endif
 #else
 #include <fstlog/detail/fstlog_ex.hpp>
 
