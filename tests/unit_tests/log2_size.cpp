956d57a11efb289ab78a890de3730580418c5abd
Dénes Derhán <denes.derhan@gmail.com>
2025-08-27 18:41:19 +0200

    log2_size fix

--- tests/unit_tests/log2_size.cpp
+++ tests/unit_tests/log2_size.cpp
@@ -2,15 +2,13 @@
 //Distributed under the AGPLv3 license (https://opensource.org/license/agpl-v3).
 #include <catch2/catch_all.hpp>
 
+#include <cstdint>
+
 #include <fstlog/detail/log2_size.hpp>
 
-TEST_CASE("log2_size_01") {
-    SECTION("01") {
-        
-        CHECK(fstlog::log2_size<char>::value == 0);
-        CHECK(fstlog::log2_size<uint16_t>::value == 1);
-        CHECK(fstlog::log2_size<uint32_t>::value == 2);
-        CHECK(fstlog::log2_size<uint64_t>::value == 3);
-    };
-
+TEST_CASE("log2_size") { 
+    CHECK(fstlog::log2_size<std::uint8_t>::value == 0);
+    CHECK(fstlog::log2_size<std::uint16_t>::value == 1);
+    CHECK(fstlog::log2_size<std::uint32_t>::value == 2);
+    CHECK(fstlog::log2_size<std::uint64_t>::value == 3);
 }
