8f006731cb6aaa5c934e83ba3de871d33d4bc184
Dénes Derhán <denes.derhan@gmail.com>
2025-08-29 10:44:40 +0200

    log2_size fix

--- fstlog/include/fstlog/detail/log2_size.hpp
+++ fstlog/include/fstlog/detail/log2_size.hpp
@@ -7,16 +7,17 @@ namespace fstlog {
     template<typename T>
     struct log2_size {
         static_assert(std::is_arithmetic_v<T>, "Unsupported type!");
-        static constexpr unsigned char value = []{
-            if constexpr (sizeof(T) == 1) return 0;
-            else if constexpr (sizeof(T) == 2) return 1;
-            else if constexpr (sizeof(T) == 4) return 2;
-            else if constexpr (sizeof(T) == 8) return 3;
-            else if constexpr (sizeof(T) == 16) return 4;
-            else {
-                static_assert(!sizeof(T), "Unsupported type!");
-                return 0;
-            }
-        }();
+        static constexpr unsigned char value = 
+            static_cast<unsigned char>([]{
+                if constexpr (sizeof(T) == 1) return 0;
+                else if constexpr (sizeof(T) == 2) return 1;
+                else if constexpr (sizeof(T) == 4) return 2;
+                else if constexpr (sizeof(T) == 8) return 3;
+                else if constexpr (sizeof(T) == 16) return 4;
+                else {
+                    static_assert(!sizeof(T), "Unsupported type!");
+                    return 0;
+                }
+            }());
     };
 }
