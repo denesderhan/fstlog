47fc68f21f2b81fea58c108caba37d24ec8cc1ee
Dénes Derhán <denes.derhan@gmail.com>
2025-09-07 10:53:21 +0200

    Refactored nearest_pow2.hpp

--- fstlog/include/fstlog/detail/is_pow2.hpp
+++ fstlog/include/fstlog/detail/is_pow2.hpp
@@ -4,8 +4,17 @@
 #include <type_traits>
 
 namespace fstlog {
+    /**
+     * @brief Checks if an unsigned integer is a power of 2
+     *
+     * @tparam T An unsigned integer type
+     * @param num The number to check
+     * @return bool true if num is a power of 2 (1, 2, 4, ...), false otherwise
+     *
+     * @note Returns false for 0, as 0 is not considered a power of 2
+     */
     template<typename T>
-    inline constexpr bool is_pow2(T num) noexcept {
+    [[nodiscard]] inline constexpr bool is_pow2(T num) noexcept {
         static_assert(std::is_unsigned_v<T>, "Only for unsigned!");
         return num && ((num & (num - 1)) == 0);
     }
--- fstlog/src/detail/nearest_pow2.hpp
+++ fstlog/src/detail/nearest_pow2.hpp
@@ -5,20 +5,38 @@
 #include <limits>
 #include <type_traits>
 
+#include <fstlog/detail/is_pow2.hpp>
+
 namespace fstlog {
     namespace detail {
+        /**
+         * @brief Computes the closest representable power of 2 
+         * to a given unsigned integer
+         *
+         * @tparam T An unsigned integer type
+         * @param num The input number
+         * @return T the closest representable power of 2 to num. In case of a tie 
+         * returns the larger power of 2
+         * @note This function is used for correcting bad (non power of 2) user input.
+         * When the input is the expected power of 2, the time complexity is O(1).
+         */
         template<typename T>
-        inline constexpr T nearest_pow2(T num) noexcept {
+        [[nodiscard]] inline constexpr T nearest_pow2(T num) noexcept {
             static_assert(std::is_unsigned_v<T>, "T can only be unsigned!");
-            if (num == 0) return 1;
-            constexpr T max_pow2 = 
-                T(1) << ((sizeof(T) * std::numeric_limits<unsigned char>::digits) - 1);
+            // fast path
+            if (is_pow2(num)) return num;
+            
+            // edge cases
+            if (num == 0 ) return 1;
+            constexpr T max_pow2 = T(1) << (std::numeric_limits<T>::digits - 1);
             if (num >= max_pow2) return max_pow2;
-            T lower = max_pow2;
-            while (lower > num) {
-                lower = lower >> 1;
-            }
-            const T upper = lower << 1;
+            
+            // Start searching from 4 since 0, 1, and 2 are handled above
+            T upper = 4;
+            while (num > upper) upper <<= 1;
+            const T lower = upper >> 1;
+
+            // return closest
             if (num - lower < upper - num) {
                 return lower;
             }
