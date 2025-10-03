83b4d6a4f6218c6f9e898dca1a25ab5de99478f5
Dénes Derhán <denes.derhan@gmail.com>
2025-10-03 03:29:31 +0200

    core init() error fix

--- fstlog/include/fstlog/core.hpp
+++ fstlog/include/fstlog/core.hpp
@@ -27,7 +27,7 @@ namespace fstlog {
             error_code error{ error_code::none };
             if (!compatible()) error = error_code::incomp_api;
             else if (!memory_resource_identical()) error = error_code::mem_res_bad;
-            else error = error = init(resource);
+            else error = init(resource);
             handle_error(error);
         }
         FSTLOG_API core(
